4adc59996843ca21e17efe29f7edaed67d3851b9
Swapnil Dhiman <58816804+swapnil0076@users.noreply.github.com>
2023-08-10 02:45:48 +0530

    Update settings.py

--- problem_02/settings.py
+++ problem_02/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'problem_02.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        "DIRS": [
+            BASE_DIR / "templates",
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
